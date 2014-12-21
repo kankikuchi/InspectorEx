0d38d0aed46d669000799413d1b074aa598e28f0
kan kikuchi <kan.kikuchi.000@gmail.com>
2014-12-21 17:23:48 +0900

    MonsterData作成

---
+++ Assets/Scripts/MonsterData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterData : ScriptableObject
+{
+	public List<Param> list = new List<Param> ();
+	
+	[System.SerializableAttribute]
+	public class Param
+	{
+		SetMonsterStatus.MonsterType Type;
+		public float HP, MP ,Power;
+		public Vector3    Position;
+		public Quaternion Rotation;
+	}
+}
+
